eb4c4dbfedf7c0ae76e58a8472905af988693020
Lisa Wilton <mishmao14@gmail.com>
2020-03-13 11:17:43 +0000

    fixed indentation

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,18 +18,18 @@ def reconstruct_trip(tickets, length):
 
     # Loop through the tickets array and create a hash table with the information
     # The starting location is the key, the destination is the value
-    for ticket in tickets:
-        hash_table_insert(hashtable, ticket.source, ticket.destination)
+    for index in tickets:
+        hash_table_insert(hashtable, index.source, index.destination)
         # the ticket with source as none is the first ticket in the list
         route[0] = hash_table_retrieve(hashtable, "NONE")
         # starting location is the key each time
         # destination is the value
         # loop through the tickets for the rest of the length of the array
-        for index in range(1, len(tickets)):
-            # then add each route to the array according to the previous entry's location
-            route[index] = hash_table_retrieve(hashtable, route[index - 1])
+    for index in range(1, len(tickets)):
+        # then add each route to the array according to the previous entry's location
+        route[index] = hash_table_retrieve(hashtable, route[index - 1])
 
-        return route    
+    return route[:-1]    
 
         
             
