e4170e07d9fbfab07cef8932f424e9823bd2a806
Lisa Wilton <mishmao14@gmail.com>
2020-03-13 11:01:13 +0000

    almost there - getting error on second hash exercise

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,12 +8,35 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    # store each weight in the input list as keys, and the weight's list index as the value in hash_table_insert(table, key, value )
+    for index in range(len(weights)):
+        hash_table_insert(ht, weights[index], index)
+
+    # loop through the weights
+    for index in range(len(weights)):
+        # assign each value to weight
+        weight = weights[index]
+        # calculate the difference between the limit and each weight
+        difference = limit - weight
+        # look to see if there is a value in the table equal to the difference
+        second_value = hash_table_retrieve(ht, difference)
+        if second_value is not None:
+            # return the higher value first each time
+            if second_value > index:
+                return (second_value, index)
+            else:
+                return (index, second_value) 
+        # if the second_value is None, return None           
+        
+    return None    
+
+
+
+
 
     """
     YOUR CODE HERE
     """
-
-    return None
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,31 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
+    # Loop through the tickets array and create a hash table with the information
+    # The starting location is the key, the destination is the value
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+        # the ticket with source as none is the first ticket in the list
+        route[0] = hash_table_retrieve(hashtable, "NONE")
+        # starting location is the key each time
+        # destination is the value
+        # loop through the tickets for the rest of the length of the array
+        for index in range(1, len(tickets)):
+            # then add each route to the array according to the previous entry's location
+            route[index] = hash_table_retrieve(hashtable, route[index - 1])
+
+        return route    
+
+        
+            
+
+
+
+
+
     """
     YOUR CODE HERE
     """
 
-    pass
+   
+
